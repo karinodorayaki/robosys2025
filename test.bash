f2334edb38449630a175f808d803338544df40c7
karinodorayaki <dorayaki.kari@gmail.com>
2025-11-06 11:09:50 +0900

    Add tests

--- test.bash
+++ test.bash
@@ -14,7 +14,7 @@ out=$(seq 5 | ./plus)
 [ "${out}" = 15 ] || ng $LINENO
 
 ### STRANGE INPUT ###
-out$(echo あ | ./plus)
+out=$(echo あ | ./plus)
 [ "$?" = 1 ]      || ng $LINENO
 [ "${out}" = "" ] || ng $LINENO
 
